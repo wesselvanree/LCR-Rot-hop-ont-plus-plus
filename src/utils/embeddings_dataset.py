5c7668f47f6d43396cd7799be839de0452725236
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2025-11-15 14:03:17 +0100

    feat: add paths util

--- src/utils/embeddings_dataset.py
+++ src/utils/embeddings_dataset.py
@@ -5,20 +5,32 @@ import torch
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 
+from src.utils.paths import Paths
+
 
 class EmbeddingsDataset(Dataset):
-    def __init__(self, year: int, phase='Train', ont_hops: Optional[int] = None, device=torch.device('cpu'),
-                 empty_ok=False, enable_cache=True, use_vm=True, use_soft_pos=True):
-        self.dir = f'data/embeddings/{year}-{phase}'
+    def __init__(
+        self,
+        year: int,
+        phase="Train",
+        ont_hops: Optional[int] = None,
+        device=torch.device("cpu"),
+        empty_ok=False,
+        enable_cache=True,
+        use_vm=True,
+        use_soft_pos=True,
+    ):
+        filename = f"{year}-{phase}"
         if ont_hops is not None:
-            self.dir += f"_hops-{ont_hops}"
+            filename += f"_hops-{ont_hops}"
         if not use_vm:
-            self.dir += f"_no-vm"
+            filename += f"_no-vm"
         if not use_soft_pos:
-            self.dir += f"_no-sp"
+            filename += f"_no-sp"
 
+        self.dir = Paths.repo_root / "data" / "embeddings" / filename
         self.device = device
-        self.length = len(glob.glob(f'{self.dir}/*.pt'))
+        self.length = len(glob.glob(f"{self.dir}/*.pt"))
         self.cache: dict[int, tuple] = {}
         self.enable_cache = enable_cache
 
@@ -30,10 +42,12 @@ class EmbeddingsDataset(Dataset):
             return self.cache[item]
 
         data: dict = torch.load(f"{self.dir}/{item}.pt", map_location=self.device)
-        label: torch.Tensor = torch.tensor(data['label'], requires_grad=False, device=self.device)
-        embeddings: torch.Tensor = data['embeddings']
-        target_pos: tuple[int, int] = data['target_pos']
-        hops: Optional[torch.Tensor] = data['hops']
+        label: torch.Tensor = torch.tensor(
+            data["label"], requires_grad=False, device=self.device
+        )
+        embeddings: torch.Tensor = data["embeddings"]
+        target_pos: tuple[int, int] = data["target_pos"]
+        hops: Optional[torch.Tensor] = data["hops"]
         target_index_start, target_index_end = target_pos
 
         left: torch.Tensor = embeddings[0:target_index_start]
@@ -43,7 +57,7 @@ class EmbeddingsDataset(Dataset):
         result = (
             (left.to(self.device), target.to(self.device), right.to(self.device)),
             label,
-            hops
+            hops,
         )
 
         if self.enable_cache:
@@ -58,13 +72,20 @@ class EmbeddingsDataset(Dataset):
         return f"EmbeddingsDataset({self.dir})"
 
 
-def train_validation_split(dataset: EmbeddingsDataset, validation_size=0.2, seed: Optional[float] = None):
+def train_validation_split(
+    dataset: EmbeddingsDataset, validation_size=0.2, seed: Optional[float] = None
+):
     # create list of all labels
     loader = DataLoader(dataset, collate_fn=lambda batch: batch)
     labels: list[int] = [data[0][1].item() for data in loader]
 
     # create stratified train-validation split
     train_idx, validation_idx = train_test_split(
-        range(len(dataset)), test_size=validation_size, shuffle=True, stratify=labels, random_state=seed)
+        range(len(dataset)),
+        test_size=validation_size,
+        shuffle=True,
+        stratify=labels,
+        random_state=seed,
+    )
 
     return train_idx, validation_idx
---
+++ src/utils/paths.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+class Paths:
+    repo_root = Path(__file__).parent.parent.parent
